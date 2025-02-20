eb8836b6ff8fde1eb297436b801822701568edad
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2025-02-20 14:59:13 +0700

    Pratikum Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+///
+Route::get('/hello', function () { 
+    return 'Hello World';
+});
+
+///
+Route::get('/world', function () 
+    { return 'World';
+});
+
+///
+Route::get('/welcome', function () 
+    { return '2341760174 Muhammad Nazril Nur Rahman';
+});
+
+// Route::get('/user/{name}', function ($name) 
+//     { return 'My name is '.$name;
+// });
+
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+    Route::get('/posts/{post}/comments/{comment}/id/{id}', function ($postId, $commentId,$id) {
+        return 'Pos ke-'.$postId." Komentar ke-: ".$commentId. "id ku yang ke-".$id;
+        });
+
+///
+Route::get('/user/{name?}', function ($name='sumanto') 
+    { return 'My name is '.$name;
+});
+
+///
